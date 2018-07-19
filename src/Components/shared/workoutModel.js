5e0b8e01667a3a8366b0ba6f5dea5c4bc402cea3
Hermaze <hermansoto@me.com>
2018-07-18 22:39:58 -0400

    Add Circuit Class and Exercise Class to workoutModel

--- src/Components/shared/workoutModel.js
+++ src/Components/shared/workoutModel.js
@@ -4,8 +4,9 @@ let workout = {
   time: '2pm',
   duration: 60,
   scheduledByTrainer: true,
-  type: 'cardio',  // [ 'regeneration', 'cardio', 'resisitance' ]
-  difficulty: 'easy', //[ 'easy', 'medium', 'hard'],
+  type: [],  // [ 'regeneration', 'cardio', 'resisitance' ]
+  difficulty: '1', //[ 'easy', 'medium', 'hard'],
+  description: '', 
   coachGoals: [ 'see what movements cause pain'],
   coachNotes: '',
   clientNotes: '',
@@ -57,4 +58,33 @@ let workout = {
       ]
     }
   ]
+}
+
+class Exercise {
+  constructor(number,tags,name,sets,reps,load,rest,tempo) {
+    this.number = number;
+    this.tags = tags;
+    this.name = name;
+    this.sets = sets;
+    this.reps = reps;
+    this.load = load;
+    this.rest = rest;
+    this.tempo = tempo;
+  }
+ 
+  
+}
+
+let exercise1 = new Exercise(1, ['DB', 'push'], 'db chest press', 3, 10, 135, '1 min', '3-2-1');
+console.log(exercise1);
+
+
+class Circuit {
+  constructor(number, type, exercises) {
+    this.number = number;
+    this.type = type;
+    this.exercise = exercise;
+  }
+
+
 }
